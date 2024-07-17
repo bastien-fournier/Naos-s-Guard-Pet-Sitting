95e56f2b62ace789431450026026d65d29c861d5
Bastien Fournier <bastien.fournier06@gmail.com>
2024-07-17 12:26:00 +0200

    add componants logo home page

---
+++ client/src/components/Logo/Logo.jsx
@@ -0,0 +1,9 @@
+import LogoNaos from "../../assets/images/logo.png";
+
+export default function Logo() {
+  return (
+    <div>
+      <img src={LogoNaos} alt="Logo" />
+    </div>
+  );
+}
--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,11 +1,17 @@
 import ButtonReview from "../components/Buttons/ButtonReview";
 import ButtonSignUp from "../components/Buttons/ButtonSignUp";
+import Logo from "../components/Logo/Logo";
 
 export default function HomePage() {
   return (
-    <div className="flex justify-center gap-7">
-      <ButtonReview />
-      <ButtonSignUp />
+    <div>
+      <div className="w-24">
+        <Logo />
+      </div>
+      <div className="flex justify-center gap-7">
+        <ButtonReview />
+        <ButtonSignUp />
+      </div>
     </div>
   );
 }
