6f4097929db84fc5d2a9b380c9dc56378ee6adff
Bastien Fournier <bastien.fournier06@gmail.com>
2024-07-19 09:14:21 +0200

    hide button sign up in home page if user is connected

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -3,9 +3,11 @@ import ButtonLogin from "../components/Buttons/ButtonLogin";
 import ButtonReview from "../components/Buttons/ButtonReview";
 import ButtonSignUp from "../components/Buttons/ButtonSignUp";
 import Logo from "../components/Logo/Logo";
+import { useUserContext } from "../components/context/UserContext";
 
 export default function HomePage() {
-  return (
+  const { user } = useUserContext();
+  return user === null || user === "null" ? (
     <div>
       <div className="flex justify-between items-center mr-3 md:hidden">
         <Logo />
@@ -17,6 +19,19 @@ export default function HomePage() {
       <div className="flex justify-center gap-7 mt-10 mb-10">
         <ButtonReview />
         <ButtonSignUp />
+      </div>
+    </div>
+  ) : (
+    <div>
+      <div className="flex justify-between items-center mr-3 md:hidden">
+        <Logo />
+        <ButtonLogin />
+      </div>
+      <div>
+        <BodyHomePage />
+      </div>
+      <div className="flex justify-center gap-7 mt-10 mb-10">
+        <ButtonReview />
       </div>
     </div>
   );
